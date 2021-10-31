25a9da2aeeeadb53c61d61078583097532e861d8
UROS <nonamen212@gmail.com>
2021-10-31 22:05:25 +0100

    Change #7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
 const listContainerWrapper = document.querySelector(".list-container-wrapper")
 const cards = document.querySelectorAll(".card")
+const cardLists = document.querySelectorAll(".list-cards")
 let x = 0
 let y = 0
 let draggedItem
 
+console.log(cardLists)
+
 window.addEventListener("load",(e)=>{
     localStorage.setItem("cardLists",listContainerWrapper.innerHTML)
     cards.forEach(card => card.setAttribute('draggable',true))
+    cardLists.forEach(list => list.innerHTML.length === 0 ? list.style.height = "100px" : null)
 })
 
 cards.forEach(card=>card.addEventListener("dragstart",(e)=>{
@@ -16,15 +20,19 @@ cards.forEach(card=>card.addEventListener("dragstart",(e)=>{
     e.dataTransfer.setDragImage(img, 0, 0);
 }),false)
 
-window.addEventListener("mousemove",(e)=>{
+window.addEventListener("mousedown",(e)=>{
+    x = e.clientX
+    y = e.clientY
 })
 
 cards.forEach(card=>card.addEventListener("drag",(e)=>{
-    console.log(e)
     e.preventDefault()
-    card.style.top = e.y + "px"
-    card.style.left = e.x  + "px"
-    console.log(card.style.top)
+    let posY = y - e.clientY
+    let posX = x - e.clientX
+    y = e.clientY
+    x = e.clientX
+    card.style.top = (card.offsetTop - posY) + "px"
+    card.style.left = (card.offsetLeft - posX)  + "px"
     card.classList.add("card-movable")
 }),false)
 
@@ -49,9 +57,14 @@ cards.forEach(card=>card.addEventListener("dragleave",(e)=>{
     }
 }),false)
 
-cards.forEach(card=>card.addEventListener("drop",(e)=>{
+cardLists.forEach(list=>list.addEventListener("dragenter",(e)=>{
     e.preventDefault()
-    card.style.background = "white"
+    list.style.background = "blue"
+}),false)
+
+cardLists.forEach(list=>list.addEventListener("drop",(e)=>{
+    e.preventDefault()
+    list.style.background = "red"
     draggedItem.parentNode.removeChild(draggedItem)
-    card.parentNode.appendChild(draggedItem)
+    list.appendChild(draggedItem)
 }),false)
