abaff4721ad3f5411f860489aaee6fa73ba0a500
UROS <nonamen212@gmail.com>
2021-10-31 12:05:55 +0100

    Change #5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log("TEST")
+const listContainerWrapper = document.querySelector(".list-container-wrapper")
+const cards = document.querySelectorAll(".card")
+
+window.addEventListener("load",(e)=>{
+    localStorage.setItem("cardLists",listContainerWrapper.innerHTML)
+})
+
+cards.forEach(card=>card.addEventListener("drag",(e)=>{
+    card.classList.add("card-movable")
+    card.style.top = e.clientY
+    card.style.right = e.clientX
+}))
+
+cards.forEach(card=>card.addEventListener("dragend",(e)=>{
+    e.preventDefault()
+    card.classList.remove("card-movable")
+}))
