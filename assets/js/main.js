7a90e8b0a35d3e1de0e6354749688a42d44e02b4
UROS <nonamen212@gmail.com>
2021-11-01 19:16:27 +0100

    Change #11

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body")
 const listContainerWrapper = document.querySelector(".list-container-wrapper")
 const cards = document.querySelectorAll(".card")
 const cardLists = document.querySelectorAll(".list-cards")
@@ -5,6 +6,7 @@ const mainContainer = document.querySelector(".main-container")
 let x = 0
 let y = 0
 let draggedItem
+let isActive = false
 
 console.log(cardLists)
 
@@ -14,46 +16,21 @@ window.addEventListener("load",(e)=>{
     /*cardLists.forEach(list => list.innerHTML.length === 0 ? list.classList.add("empty-list") : null)*/
 })
 
-cards.forEach(card=>card.addEventListener("dragstart",(e)=>{
-    draggedItem = e.target
-    let img = new Image()
-    img.url = "../pixel.png"
-    e.dataTransfer.setData('text/plain',e.target.id)
-    e.dataTransfer.setDragImage(img, 0, 0);
-}),false)
+cards.forEach(card => card.addEventListener('mousedown',(e)=>{
+}))
 
-cards.forEach(card=>card.addEventListener("mousedown",(e)=>{
-    x = e.clientX
-    y = e.clientY
-}),false)
-
-cards.forEach(card=>card.addEventListener("mousemove",(e)=>{
-}),false)
-
-cards.forEach(card=>card.addEventListener("drag",(e)=>{
-    e.preventDefault()
+cards.forEach(card => card.addEventListener('drag',(e)=>{
     card.classList.add("card-movable")
-    let posY = y - e.clientY
-    let posX = x - e.clientX
-    y = e.clientY
-    x = e.clientX
-    /*card.style.top = (card.offsetTop - posY) + "px"
-    card.style.left = (card.offsetLeft - posX)  + "px"*/
-    /*ODREKAO GA SE U NOVINAMA*/
-    /*card.parentNode.removeChild(draggedItem)*/
-}),false)
+}))
 
-cards.forEach(card=>card.addEventListener("dragend",(e)=>{
-    e.preventDefault()
+cards.forEach(card => card.addEventListener('dragstart',(e)=>{
+    draggedItem = card
+}))
+
+cards.forEach(card => card.addEventListener('dragend',(e)=>{
     card.classList.remove("card-movable")
-}),false)
+}))
 
-cardLists.forEach(list=>list.addEventListener("dragover",(e)=>{
-    e.preventDefault()
+cardLists.forEach(list => list.addEventListener('dragover',(e)=>{
     list.appendChild(draggedItem)
-}),false)
-
-cardLists.forEach(list=>list.addEventListener("drop",(e)=>{
-    e.preventDefault()
-    /*list.classList.remove("empty-list")*/
-}),false)
+}))
